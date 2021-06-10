a618bc3dca7c855f37543fc2e6863faecbb2ce0e
Antonín Vondrovic <antonin.vondrovic@educanet.cz>
2021-06-10 21:06:57 +0200

    ano

--- src/main/java/cz/educanet/gui/Game.java
+++ src/main/java/cz/educanet/gui/Game.java
@@ -2,10 +2,12 @@ package cz.educanet.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -21,33 +23,6 @@ public class Game extends Application {
         primaryStage.setScene(new Scene(rootNode, 1175, 720));
         primaryStage.show();
     }
-
-    public void switchScene(String fxmlFile)
-    {
-
-        FXMLLoader loader = new FXMLLoader(getClass()
-                .getResource(fxmlFile));
-        Parent root;
-        try
-        {
-            root = (Parent)loader.load();
-            if(fxmlFile.equals("calculator.fxml"))
-            {
-                MenuController menucon = (MenuController)loader.getController();
-                menucon.setModel(new MenuController(menucon));
-                menucon.setLogic(this);
-            }
-            else if(fxmlFile.equals("TestSwitch.fxml"))
-            {
-                TestSwitch controller = (TestSwitch)loader.getController();
-                controller.setLogic(this);
-            }
-            this.stage.setScene(new Scene(root));
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/cz/educanet/gui/MenuController.java
+++ src/main/java/cz/educanet/gui/MenuController.java
@@ -2,9 +2,21 @@ package cz.educanet.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MenuController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private Button buttonStart;
@@ -14,20 +26,32 @@ public class MenuController {
     private Button buttonExit;
 
     // Main Menu
-    public void onClickedStart(ActionEvent actionEvent) {
-        buttonStart.setText("Drz hubu!!!");
+    public void onClickedStart(ActionEvent event) throws IOException {
+        switchScene(event, "/rooms/room0.fxml");
     }
 
-    public void onClickedOptions(ActionEvent actionEvent) {
-        buttonOptions.setText("Options open");
+    public void onClickedOptions(ActionEvent event) throws IOException {
+        switchScene(event, "/rooms/options.fxml");
     }
 
-    public void onClickedExit(ActionEvent actionEvent) {
+    public void onClickedExit(ActionEvent event) {
         System.exit(0);
+    }
+
+    public void switchScene(ActionEvent event, String file) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(file)));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Text Adventure Game");
+        stage.setScene(scene);
+        stage.show();
     }
 
     // Options
 
+    public void returnToMenu(ActionEvent event) throws IOException {
+        switchScene(event, "/mainMenu.fxml");
+    }
 
     // Character screen
 }
